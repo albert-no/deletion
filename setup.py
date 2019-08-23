8741ed57c8946c49993da3e4876f1ab1e8c33e8d
Albert <albert.no0@gmail.com>
2019-08-23 11:34:11 +0900

    FIX: add deletion_lp to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from distuitls.core import setup
 
 setup(name='deletion',
         version='1.0',
-        packages=['deletion_utils',]
+        packages=['deletion_utils',
+            'deletion_lp',]
 )
